e3104b53ceb4850871b99ed4f643c2b6335abe23
Zakyotsu <florian.hochart17@gmail.com>
2022-11-26 00:39:42 +0100

    main: hotfix sql

--- carpooling-base/tables.sql
+++ carpooling-base/tables.sql
@@ -20,10 +20,12 @@ CREATE TABLE `cars` (
 
 INSERT INTO `cars` (`id`, `brand`, `model`, `mineral`, `color`, `nbrSlots`) VALUES
 (1,	'BMW',	'530d',	'AA-999-ZZ',	'Red',	5),
-(2,	'Porsche',	'911',	'WE-465-LK',	'Green',	4);
+(2,	'Renault',	'21',	'AG-647-HG',	'Gray',	5),
+(5,	'Nissan',	'GTR',	'FE-617-GQ',	'Purple',	5),
+(6,	'BMW',	'525d e60',	'WW-515-PF',	'Black',	5);
 
-DROP TABLE IF EXISTS `offer`;
-CREATE TABLE `offer` (
+DROP TABLE IF EXISTS `offers`;
+CREATE TABLE `offers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `idCar` int NOT NULL,
   `idPublisher` int NOT NULL,
@@ -31,25 +33,36 @@ CREATE TABLE `offer` (
   `price` int NOT NULL,
   `locationFrom` varchar(255) NOT NULL,
   `locationTo` varchar(255) NOT NULL,
-  `dateDepart` date NOT NULL,
-  `dateArrival` date NOT NULL,
+  `dateDepart` datetime NOT NULL,
+  `dateArrival` datetime NOT NULL,
   `isAvailable` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idCar` (`idCar`),
+  KEY `idPublisher` (`idPublisher`),
+  CONSTRAINT `offers_ibfk_1` FOREIGN KEY (`idCar`) REFERENCES `cars` (`id`),
+  CONSTRAINT `offers_ibfk_2` FOREIGN KEY (`idPublisher`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `offers` (`id`, `idCar`, `idPublisher`, `name`, `price`, `locationFrom`, `locationTo`, `dateDepart`, `dateArrival`, `isAvailable`) VALUES
+(1,	1,	2,	'Paris -> Nantes',	25,	'Paris',	'Nantes',	'2010-11-08 11:00:00',	'2010-11-08 13:30:00',	1),
+(2,	5,	9,	'Little trip',	100,	'Limoges',	'Cologne',	'2022-11-25 10:00:00',	'2022-11-25 23:30:00',	1);
 
-DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE `reservation` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE `reservations` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `idOffer` int NOT NULL,
   `idBuyer` int NOT NULL,
   `isPaid` tinyint(1) NOT NULL,
   KEY `id` (`id`),
   KEY `idBuyer` (`idBuyer`),
-  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`id`) REFERENCES `offer` (`id`),
-  CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`idBuyer`) REFERENCES `users` (`id`)
+  KEY `idOffer` (`idOffer`),
+  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`idBuyer`) REFERENCES `users` (`id`),
+  CONSTRAINT `reservations_ibfk_3` FOREIGN KEY (`idOffer`) REFERENCES `offers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `reservations` (`id`, `idOffer`, `idBuyer`, `isPaid`) VALUES
+(2,	2,	7,	0),
+(3,	1,	2,	0);
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -62,9 +75,9 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
-(1,	'Jean',	'Valjean',	'jeanvaljean@jvj.fr',	'1982-01-09 01:00:00'),
-(2,	'Luc',	'L',	'lucl@test.fr',	'2010-11-08 01:00:00'),
-(3,	'Florian',	'H',	'fh@fh.fr',	'2000-09-08 02:00:00');
+(2,	'Jean',	'Valjean',	'jeanvaljean@jvj.fr',	'1982-01-09 01:00:00'),
+(7,	'Luc',	'L',	'lucl@test.fr',	'2010-11-08 01:00:00'),
+(9,	'Florian',	'H',	'fh@fh.fr',	'2000-09-08 02:00:00');
 
 DROP TABLE IF EXISTS `users_cars`;
 CREATE TABLE `users_cars` (
@@ -77,8 +90,8 @@ CREATE TABLE `users_cars` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `users_cars` (`user_id`, `car_id`) VALUES
-(1,	1),
-(2,	1),
-(2,	2);
+(7,	1),
+(9,	1),
+(9,	2);
 
--- 2022-11-23 17:55:23
+-- 2022-11-25 23:39:10
